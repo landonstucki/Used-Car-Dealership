89011ab76064eb59950a240620b7f104c237aef5
Landon <Landon@users.noreply.github.com>
2026-03-06 15:15:44 -0700

    Node Updated

--- server.js
+++ server.js
@@ -2,6 +2,8 @@
 import { fileURLToPath } from 'url';
 import path from 'path';
 import express from 'express';
+import livereload from 'livereload';
+import connectLivereload from 'connect-livereload';
 
 // Variable Declaration
 const __filename = fileURLToPath(import.meta.url);
@@ -11,10 +13,22 @@ const PORT = process.env.PORT || 3000;
 // Setup Express Server
 const app = express();
 
+// Live Reload Server
+const liveReloadServer = livereload.createServer();
+liveReloadServer.watch([
+    path.join(__dirname, 'public'),
+    path.join(__dirname, 'src/views')
+]);
+
+liveReloadServer.server.once('connection', () => {
+    setTimeout(() => {
+        liveReloadServer.refresh('/');
+    }, 100);
+});
 
 // Middleware
+app.use(connectLivereload());
 app.use(express.static(path.join(__dirname, 'public')));
-
 
 // Route Declaration
 app.get('/', (req, res) => {
