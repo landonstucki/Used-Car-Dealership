3b3931b41ade3355515d59955f5d2f43d83e23e0
Landon <Landon@users.noreply.github.com>
2026-04-01 16:10:16 -0600

    Database Overload Protection

--- src/config/db.js
+++ src/config/db.js
@@ -5,7 +5,8 @@ const { Pool } = pg;
 
 const pool = new Pool({
   connectionString: process.env.DB_URL,
-  ssl: { rejectUnauthorized: false }
+  ssl: { rejectUnauthorized: false },
+  max: 2, // leave 1 slot for pgAdmin login
 });
 
 export default pool;
