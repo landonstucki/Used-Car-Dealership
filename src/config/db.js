123ad5a4b48e670f5004cc60d2af3342b2bf6e41
Landon <Landon@users.noreply.github.com>
2026-04-03 18:20:15 -0600

    Testing Database Connection

--- src/config/db.js
+++ src/config/db.js
@@ -1,16 +1,15 @@
 import 'dotenv/config';
-import pg from 'pg';
+import pg from "pg";
 
 const { Pool } = pg;
 
-console.log('DB_URL exists:', !!process.env.DB_URL);
-console.log('NODE_ENV:', process.env.NODE_ENV);
-
 const pool = new Pool({
   connectionString: process.env.DB_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
+  ssl: { rejectUnauthorized: false },
+  max: 1, // leave 1 slot for pgAdmin login
+  idleTimeoutMillis: 5000,
+  connectionTimeoutMillis: 10000
+  
 });
 
 export default pool;
